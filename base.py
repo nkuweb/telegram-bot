a1f77a222854a0769432e0a251b792612a97e7ff
utkucanbykl <utkucanbykl@gmail.com>
2017-05-03 02:43:20 +0300

    Fixed Group Bugs

--- base.py
+++ base.py
@@ -6,9 +6,9 @@ updater = Updater(token=Token)
 dispatcher = updater.dispatcher
 
 def start(bot,update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Bot çalışıyor hacı :D")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Bot çalışıyor hacı")
 def hello(bot,update):
-    bot.sendMessage(chat_id=update.message.chat_id,text="Hello")
+    bot.sendMessage(chat_id=update.message.chat_id,text="Hello"+update.message.from_user.first_name)
 def echo(bot,update):
     if(check.url(update.message.text) != False):
         bot.sendMessage(chat_id=update.message.chat_id, text="Databaseye kaydettim")
@@ -16,6 +16,12 @@ def echo(bot,update):
         bot.sendMessage(chat_id=update.message.chat_id, text="Sanada Hello")
     else:
         bot.sendMessage(chat_id=update.message.chat_id, text="Boş Konuşma "+update.message.from_user.first_name)
+def kaynak(bot,update):
+    if (check.url(update.message.text) != False):
+        bot.sendMessage(chat_id=update.message.chat_id, text="Databaseye kaydettim")
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id,text="Url yanlış.")
+
 
 
 #---------------HANDLER IS HERE--------------------
@@ -23,6 +29,7 @@ def echo(bot,update):
 start_handler = CommandHandler('start', start)
 hello_handler=CommandHandler('hello',hello)
 echo_handler = MessageHandler(Filters.text, echo)
+kaynak_handler=CommandHandler('kaynak',kaynak)
 
 #--------------------------------------------------
 #----------------DISPATCHER IS HERE----------------
@@ -30,6 +37,7 @@ echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(hello_handler)
+dispatcher.add_handler(kaynak_handler)
 
 #--------------------------------------------------
 

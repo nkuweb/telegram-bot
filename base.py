cb06d47a5bb6ed5004a0b2fc0a7d620496825c75
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2017-05-03 13:58:08 +0300

    Update base.py

--- base.py
+++ base.py
@@ -8,7 +8,7 @@ updater = Updater(token=Token)
 dispatcher = updater.dispatcher
 
 def start(bot,update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Bot çalışıyor hacı")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Bot çalışıyor.")
 def hello(bot,update):
     bot.sendMessage(chat_id=update.message.chat_id,text="Hello"+update.message.from_user.first_name)
 def echo(bot,update):
@@ -18,25 +18,23 @@ def echo(bot,update):
         bot.sendMessage(chat_id=update.message.chat_id, text="Sanada Hello")
     else:
         bot.sendMessage(chat_id=update.message.chat_id, text="Boş Konuşma "+update.message.from_user.first_name)
-def kaynak2(bot,update):
+def kaynak(bot,update):
 
     msg = update.message.text
     x = str(msg ).replace("/kaynak"," ")
     k =  x.split(" ")
     a=check.url(k[2])
 
-    print(update.message.text)
-
     if (a == True):
-        bot.sendMessage(chat_id=update.message.chat_id, text="Url eklendi")
-        readme= open('README.md', 'a')
+	bot.sendMessage(chat_id=update.message.chat_id, text=update.message.from_user.first_name+"'nin Kaynağı Databaseye kaydettim")
+	readme= open('README.md', 'a')
         x = str(update.message.text).replace("/kaynak"," ")
-        readme.write("* [{}]\n".format(x))
+        readme.write("* [{}]\n".format(update.message.from_user.first_name+"'nin "+x))
         readme.close()
         os.system("git add -A")
         os.system("git commit -m 'Link'")
         os.system("git push")
-	bot.sendMessage(chat_id=update.message.chat_id,text="URL ALNIMIZIN AKIYLA EKLENDİ.")
+	bot.sendMessage(chat_id=update.message.chat_id,text="Url github'a eklendi.")
     else:
         bot.sendMessage(chat_id=update.message.chat_id,text="URL HATALI")
 
@@ -47,7 +45,7 @@ def kaynak2(bot,update):
 start_handler = CommandHandler('start', start)
 hello_handler=CommandHandler('hello',hello)
 echo_handler = MessageHandler(Filters.text, echo)
-kaynak_handler=CommandHandler('kaynak2',kaynak2)
+kaynak_handler=CommandHandler('kaynak',kaynak)
 
 #--------------------------------------------------
 #----------------DISPATCHER IS HERE----------------
